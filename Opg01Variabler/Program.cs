8d36f44818c623b3457e44e2dc78f3b983961c24
jesperrc <jesper.christophersen@gmail.com>
2018-03-08 15:48:52 +0100

    Færdig med opgaven

--- Opg01Variabler/Program.cs
+++ Opg01Variabler/Program.cs
@@ -7,11 +7,30 @@ namespace Opg01Variabler
         static void Main(string[] args)
         {
 
-              
-            // Your code here.....
+            int a = 10;
+            a++;
+            a--;
+            a += 20;
+            Console.WriteLine(a);
 
+            double b = 12.5;
+            b++;
+            b--;
+            b *= 2;
+            Console.WriteLine(b);
 
+            FilType ft = FilType.csv;
+            Console.WriteLine(ft);
+            //Udskriver værdien i stedet for strengen
+            Console.WriteLine(Convert.ToInt32(ft));
 
+            DateTime dt = DateTime.Now;
+            Console.WriteLine(dt.ToString("ddMMyy"));
+
+            Person p = new Person();
+            p.Id = 1;
+            p.Navn = "Mikkel";
+            Console.WriteLine(p.Navn);
 
             // Keep console window open when using the debugger (F5)
             if (System.Diagnostics.Debugger.IsAttached)
@@ -21,4 +40,18 @@ namespace Opg01Variabler
             }
         }
     }
+
+    enum FilType
+    {
+        csv,
+        pdf,
+        txt
+    }
+
+    struct Person
+    {
+        public int Id;
+        public string Navn;
+    }
+
 }
