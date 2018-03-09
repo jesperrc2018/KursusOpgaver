8cd20c361d99db2b5678522f0c92d0ce82c274cc
jesperrc <jesper.christophersen@gmail.com>
2018-03-09 10:24:37 +0100

    .

---
+++ Array/Program.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // Your code here.....
+
+
+
+
+            // Keep console window open when using the debugger (F5)
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Opg02Array/Program.cs
@@ -0,0 +1,49 @@
+using System;
+
+namespace Opg02Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] månedsløn = { 10000, 20000, 15000, 20000 , 30000,15000 };
+
+            double sum = 0;
+
+            Console.WriteLine("Usorteret!");
+            foreach (var løn in månedsløn)
+            {
+                sum += løn;
+                Console.WriteLine(løn);
+            }
+
+            Console.WriteLine("Gennemsnit!");
+            Console.WriteLine(sum/månedsløn.Length);
+
+            Console.WriteLine("Sorteret!");
+
+            System.Array.Sort(månedsløn);
+
+            foreach (var løn in månedsløn)
+            {
+                sum += løn;
+                Console.WriteLine(løn);
+            }
+
+
+
+            // Your code here.....
+
+
+
+
+            // Keep console window open when using the debugger (F5)
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+    }
+}
